ab04a4936be70794ba5362729e7fec46772c2a0c
JinHyeok00 <kai6879.dev@gmail.com>
2023-12-21 20:47:39 +0900

    1221 2047

--- member/src/main/java/com/app/member/controller/MemberController.java
+++ member/src/main/java/com/app/member/controller/MemberController.java
@@ -5,10 +5,7 @@ import com.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -90,5 +87,11 @@ public class MemberController {
     public String logout(HttpSession session){
         session.invalidate();
         return "index";
+    }
+
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail")String memberEmail){
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
     }
 }
